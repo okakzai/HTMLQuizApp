3a5f8f29192494e8eca34b00fb2db606b6fcb830
Zainal Abidin <okakzai@gmail.com>
2016-11-24 22:51:10 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,62 +1,79 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        
-		admob.setOptions({
-			publisherId: "ca-app-pub-2992495660183815/6015103080",
-			interstitialAdId: "ca-app-pub-2992495660183815/7491836280",
-			bannerAtTop: true
-		});
-		admob.createBannerView();
-		admob.requestIntertitial();
-		
-		//app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        /*
-		var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var isAppForeground = true;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-		*/
+function onAdLoaded(e) {
+  if (isAppForeground) {
+    if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+      admob.showInterstitialAd();
     }
-};
+  }
+}
 
-app.initialize();
+function onAdClosed(e) {
+  if (isAppForeground) {
+    if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+      setTimeout(admob.requestInterstitialAd, 1000 * 60 * 2);
+    }
+  }
+}
+
+function onPause() {
+  if (isAppForeground) {
+    admob.destroyBannerView();
+    isAppForeground = false;
+  }
+}
+
+function onResume() {
+  if (!isAppForeground) {
+    setTimeout(admob.createBannerView, 1);
+    setTimeout(admob.requestInterstitialAd, 1);
+    isAppForeground = true;
+  }
+}
+
+// optional, in case respond to events
+function registerAdEvents() {
+  document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+  document.addEventListener(admob.events.onAdClosed, onAdClosed);
+
+  document.addEventListener("pause", onPause, false);
+  document.addEventListener("resume", onResume, false);
+}
+
+function initAds() {
+  if (admob) {
+    var adPublisherIds = {
+        android : {
+        banner : "ca-app-pub-2992495660183815/6015103080",
+        interstitial : "ca-app-pub-2992495660183815/7491836280"
+      }
+    };
+
+    var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+
+    admob.setOptions({
+      publisherId:          admobid.banner,
+      interstitialAdId:     admobid.interstitial,
+      bannerAtTop: true,	    
+      autoShowInterstitial: false
+    });
+
+    registerAdEvents();
+
+  } else {
+    alert('AdMobAds plugin not ready');
+  }
+}
+
+function onDeviceReady() {
+  document.removeEventListener('deviceready', onDeviceReady, false);
+  initAds();
+
+  // display a banner at startup
+  admob.createBannerView();
+
+  // request an interstitial
+  admob.requestInterstitialAd();
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
